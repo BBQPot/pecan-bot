7d98c01dc6d90763dd98becd1d623e5a460600cb
Anti-Tank Cat <mhz11235@gmail.com>
2022-01-07 17:12:53 -0600

    implementation of UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import pecan from './res/logo_pecan.png';
 import logoText from './res/logo_text.png';
-import { Checkbox, FormGroup, FormControlLabel, FormControl, FormLabel, Radio, RadioGroup} from '@mui/material';
+import { Button, TextField, Typography, Checkbox, FormGroup, FormControlLabel, FormControl, FormLabel, Radio, RadioGroup } from '@mui/material';
 import * as React from 'react';
+import Box from '@mui/material/Box';
 
 const eveSsoIcon = "https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-white-large.png"
 const callback = "https://bbqpot.github.io/pecan-bot";
@@ -30,27 +31,73 @@ function App() {
 
 function MailPage() {
     const [srpStatus, setSrpStatus] = React.useState("rejected");
+    const [srpPing, setSrpPing] = React.useState(false);
+    const [srpShip, setSrpShip] = React.useState(false);
+    const [srpFit, setSrpFit] = React.useState(false);
+    const [srpSplit, setSrpSplit] = React.useState(false);
+    const [srpEqip, setSrpEqip] = React.useState(false);
+    const [recipient, setRecipient] = React.useState("");
+
     return (
-        <body>
+        <body className="App-background">
             <div className="App-title">
                  <img src = {pecan} className="App-logo-small"/>
                  <img src = {logoText} className="App-logo-small"/>
             </div>
-            <div id="body" className="App-body">
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">SRP Status</FormLabel>
-                    <RadioGroup
-                        row
-                        aria-label="srp-status"
-                        defaultValue="rejected"
-                        onChange={(event) => setSrpStatus(event.target.value)}
-                    >
-                        <FormControlLabel value="rejected" control={<Radio />} label="Rejected" />
-                        <FormControlLabel value="reduced" control={<Radio />} label="Reduced (50%)" />
-                        <FormControlLabel value="other" control={<Radio />} label="Other" />
-                    </RadioGroup>
-                </FormControl>
-                {srpStatus}
+            <div className="App-body">
+                <Box className="Hori-style">
+                    <Box className="Vertical-style" sx={{m:2}}>
+                        <TextField label="Recipient" variant="outlined" sx={{mb:2}}/>
+                        <TextField label="ZKB Link" variant="outlined"/>
+                    </Box>
+                    <FormControl sx={{m:2}}>
+                        <FormLabel component="legend">SRP Status</FormLabel>
+                        <RadioGroup
+                            aria-label="srp-status"
+                            defaultValue="rejected"
+                            onChange={(event) => setSrpStatus(event.target.value)}
+                        >
+                            <FormControlLabel value="rejected" control={<Radio />} label="Rejected" />
+                            <FormControlLabel value="reduced" control={<Radio />} label="Reduced (50%)" />
+                        </RadioGroup>
+                    </FormControl>
+                </Box>
+                <Box sx={{ pt:2}}>
+                    <FormControl>
+                        <FormLabel component="legend">With the following reason</FormLabel>
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox />} 
+                                onChange={() => setSrpPing(!srpPing)}
+                                label="Lack of or inaccurate ping info" />
+                            <FormControlLabel control={<Checkbox />} 
+                                onChange={() => setSrpShip(!srpShip)}
+                                label="Unsuitable ship" />
+                            <FormControlLabel control={<Checkbox />} 
+                                onChange={() => setSrpFit(!srpFit)}
+                                label="Inaccurate or inappropriate fitting" />
+                            <FormControlLabel control={<Checkbox />} 
+                                onChange={() => setSrpSplit(!srpSplit)}
+                                label="Splited usage of armament" />
+                            <FormControlLabel control={<Checkbox />} 
+                                onChange={() => setSrpEqip(!srpEqip)}
+                                label="Lack of required ammunation/equipment in cabin" />
+                        </FormGroup>
+                    </FormControl>
+                </Box>
+                <Box sx={{ width: '100%', maxWidth: 500, pt:2}}>
+                    <Typography> Your SRP application is {srpStatus} due to the following reason: </Typography>
+                    <p/>
+                    { srpPing && <Typography> Lack of or inaccurate ping info </Typography> }
+                    { srpShip && <Typography> Unsuitable ship </Typography> }
+                    { srpFit && <Typography> Inaccurate or inappropriate fitting </Typography> }
+                    { srpSplit && <Typography> Splited usage of armament </Typography> }
+                    { srpEqip && <Typography> Lack of required ammunation/equipment in cabin </Typography> }
+                    <p/>
+                    <Typography> Should you have any questions, please contact the SRP team by replying this in-game mail. </Typography>
+                </Box>
+                <Button variant="contained" color="error">
+                    Send!
+                </Button>
             </div>
         </body>
     );
